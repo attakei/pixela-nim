9fb8ef493e893bb7dabf1573339b746bf8232db7
Kazuya Takei <myself@attakei.net>
2020-10-04 22:51:38 +0900

    Combine to cli

--- src/pixela/cli.nim
+++ src/pixela/cli.nim
@@ -5,10 +5,13 @@ This module is CLI of reference implement to call some API endpoint.
 - All consts, types and procs are private. Don't import from outside.
 - Error handling is not implemented.
 ]##
+import json
 import os
 import parseopt
 import strutils
-import "./api/graph" as graph_api
+import "./api/graph_create"
+import "./api/graph_delete"
+import "./api/graph_get"
 import "./client"
 
 
@@ -17,6 +20,8 @@ type
     username: string
     token: string
     command: string
+    graphId: string
+    src: string
 
 
 proc parseArguments(args: var Arguments, cmdLine: string) =
@@ -30,6 +35,8 @@ proc parseArguments(args: var Arguments, cmdLine: string) =
       case p.key:
       of "user", "u": args.username = p.val
       of "token", "t": args.token = p.val
+      of "src", "s": args.src = p.val
+      of "graph", "g": args.graphId = p.val
     of cmdArgument:
       if args.command == "":
         args.command = p.key
@@ -43,5 +50,12 @@ when isMainModule:
   of "graph:list":
     for g in apiClient.getGraphs():
       echo(g.id)
+  of "graph:create":
+    let body = newGraphCreation(parseJson(readAll(open(args.src))))
+    let resp = apiClient.postGraph(body)
+    echo(resp.message)
+  of "graph:delete":
+    let resp = apiClient.deleteGraph(args.graphId)
+    echo(resp.message)
   else:
     echo("Command not found")
